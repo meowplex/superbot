435641f9a177401508741c3adef096fa634b658e
kython-c <77231491+kython-c@users.noreply.github.com>
2021-09-22 14:59:54 +0300

    add get/set/has in caching service

--- src/services/caching_service.js
+++ src/services/caching_service.js
@@ -1,8 +1,10 @@
+import { Collection } from "../model/collection";
+
 export class CachingService {
     /**
      * Mab object that stores the objects
      */
-    cache = new Map();
+    cache = new Collection();
 
     /**
      * Limit on the number of stored objects 
@@ -16,6 +18,35 @@ export class CachingService {
 
     constructor(limit){
         limit = limit;
+    }
+
+    /**
+     * @param {String} key 
+     * @returns value for the key
+     */
+    get(key){
+        return this.cache.get(key);
+    }
+
+    /**
+     * @param {String} key 
+     * @param {JSON} value 
+     */
+    set(key, value){
+        count+=1;
+        if (count >= limit){
+            this.cache.delete(this.cache.lastKey());
+            count-=1;
+        }
+        this.cache.set(key, value);
+    }
+
+    /**
+     * @param {String} key 
+     * @returns boolean - is there an element with a this key 
+     */
+    has(key){
+        return this.cache.has(key)
     }
 
 
