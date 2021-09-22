0e4f240d65223ddb61be03853de67da88fb11dc1
kython-c <77231491+kython-c@users.noreply.github.com>
2021-09-22 15:24:20 +0300

    add constructor for user

--- src/model/user.js
+++ src/model/user.js
@@ -3,6 +3,23 @@ export class User {
     id;
     username;
 
+    /**
+     * @param {int} id 
+     * @param {String} username 
+     */
+    constructor(id, username){
+        this.id = id;
+        this.username = username;
+    }
+
+    /**
+     * @param {JSON} object 
+     * @returns User object
+     */
+    static fromJSON(object){
+        return new User(object.id, object.username);
+    }
+
     toJSON() {
         return {
             'id': this.id,
