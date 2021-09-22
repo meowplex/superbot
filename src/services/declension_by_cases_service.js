0358b0c60abb2838b00f1713f616d07eb1abe5b2
kython-c <77231491+kython-c@users.noreply.github.com>
2021-09-23 00:48:11 +0300

    test: create test for a declension by cases

--- src/services/declension_by_cases_service.js
+++ src/services/declension_by_cases_service.js
@@ -9,13 +9,13 @@ export class DeclensionByCases {
      * @param {String} five declension with "three"
      * @returns case phrase
      */
-    static nounWithNumeral(numeral, one, two, five){
+    static nounWithNumeral(numeral, one, two, five) {
         let noun;
         let lastDigit = numeral % 10;
 
         if (lastDigit == 1)
             noun = one;
-        else if (lastDigit == 0 || lastDigit > 5)
+        else if (lastDigit == 0 || lastDigit >= 5)
             noun = five;
         else if (lastDigit > 1 && lastDigit < 5)
             noun = two;
