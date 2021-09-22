5b019e74708c1ce2fdaf10853f824d9a7d826232
kython-c <77231491+kython-c@users.noreply.github.com>
2021-09-22 14:47:43 +0300

    create collection

---
+++ src/model/collection.js
@@ -0,0 +1,20 @@
+export class Collection extends Map {
+
+    /**
+     * 
+     * @returns the first key(s) in this collection.
+     */
+
+    firstKey(){
+        return this.keys().next().value;
+    }
+
+    /**
+     * 
+     * @returns the last key(s) in this collection.
+     */
+    lastKey(){
+        const arrayKeys = [...this.keys()];
+        return arrayKeys[arrayKeys.length - 1];
+    }
+}
