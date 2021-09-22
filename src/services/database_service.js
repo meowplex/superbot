b437ddae87f916e031e05f8fb457abf47b3f7f45
kython-c <77231491+kython-c@users.noreply.github.com>
2021-09-22 20:39:47 +0300

    docs: add comments to database service

--- src/services/database_service.js
+++ src/services/database_service.js
@@ -14,17 +14,33 @@ export class Database {
             throw Exception("mongoose dont connected")
     }
 
+    /**
+     * 
+     * @param {JSON} params 
+     * @returns Database entry with these parameters 
+     */
     async get(params) {
         const response = await UserModel.findOne(params);
         return response;
     }
 
+    /**
+     * 
+     * @param {JSON} params 
+     * @returns The created entry in the database
+     */
     async create(params) {
         const newUser = new UserModel(params);
         await newUser.save();
         return newUser;
     }
 
+    /**
+     * 
+     * @param {JSON} oldParams 
+     * @param {JSON} newParams 
+     * @returns The updated entry in the database
+     */
     async update(oldParams, newParams) {
         const response = await UserModel.updateOne(oldParams, newParams);
         return response;
