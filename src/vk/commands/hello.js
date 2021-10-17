7cb800e927e8a03259b2e87fb66bbb8196f3f6d8
kython-c <77231491+kython-c@users.noreply.github.com>
2021-10-17 17:41:15 +0300

    refactor: rename get_hello_action to vk_hello_command

--- src/vk/commands/hello.js
+++ src/vk/commands/hello.js
@@ -3,12 +3,8 @@ import { superBot } from "../../superbot/index.js";
 import { CommandNames } from "../../../utils/command_names.js";
 import { Messengers } from "../../../utils/messengers.js";
 
-export function get_hello_action()
-{
-    return async(context) =>
-    {
-        let ctx = new Context(Messengers.vk, context.senderId)
-        let res = await superBot.run(CommandNames.hello, ctx)
-        return context.send(`${res.text}, VK`);
-    };
+export async function vk_hello_command(context) {
+    let ctx = new Context(Messengers.vk, context.senderId)
+    let res = await superBot.run(CommandNames.hello, ctx)
+    return context.send(`${res.text}, VK`);
 }
--- src/vk/index.js
+++ src/vk/index.js
@@ -1,6 +1,6 @@
 import { VK } from 'vk-io';
 import { HearManager } from '@vk-io/hear';
-import { get_hello_action } from "./commands/hello.js";
+import { vk_hello_command } from "./commands/hello.js";
 
 export const vkBot = new VK({
     token: process.env.VK_TOKEN
@@ -8,12 +8,9 @@ export const vkBot = new VK({
 
 export const hearManager = new HearManager();
 
-export function startVkBot()
-{
-    // middlewares
+export function startVkBot() {
     vkBot.updates.on('message', hearManager.middleware);
-    // commands
-    hearManager.hear(/^(?:hello|привет)$/i, get_hello_action())
+    hearManager.hear(/^(?:hello|привет)$/i, vk_hello_command)
 
     // start polling
     vkBot.updates.start().catch(err => console.error('VK Bot launch error:', err));
