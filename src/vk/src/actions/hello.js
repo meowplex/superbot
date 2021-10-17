b712e9ea84c20c9fbf02e8f8bbd15ba30febdf24
Trard <trardone@gmail.com>
2021-10-17 16:42:47 +0300

    triggers -> command names

--- src/vk/src/actions/hello.js
+++ src/vk/src/actions/hello.js
@@ -1,6 +1,6 @@
 import { Context } from "../../../../lib/superbot/index.js";
 import { superBot } from "../../../superbot/index.js";
-import { Triggers } from "../../../../utils/triggers.js";
+import { Command_names } from "../../../../utils/command_names.js";
 import { Messengers } from "../../../../utils/messengers.js";
 
 export function get_hello_action()
@@ -8,7 +8,7 @@ export function get_hello_action()
     return async(context) =>
     {
         let ctx = new Context(Messengers.vk, context.senderId)
-        let res = await superBot.run(Triggers.hello, ctx)
+        let res = await superBot.run(Command_names.hello, ctx)
         return context.send(`${res.text}, VK`);
     };
 }
