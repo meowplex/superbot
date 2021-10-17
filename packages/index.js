06a2a84936207d6a35b43f145f8d912407d4ab8c
Trard <trardone@gmail.com>
2021-10-17 20:45:32 +0300

    all works now

--- packages/index.js
+++ packages/index.js
@@ -1,15 +1,21 @@
 //import mongoose from "mongoose"
-import { start_vk_bot } from "./vk/src/index.js";
-import { start_tg_bot } from "./tg/src/index.js";
-
+import { vkBot } from "./vk/src/index.js";
+import { tgBot } from "./tg/src/index.js";
 
 const main = async () => {
     // connect to db
     //await mongoose.connect(process.env.MONGODB_URL);
 
     // start bots
-    start_tg_bot()
-    start_vk_bot();
+    tgBot.launch().catch(err => console.error('TG Bot launch error:', err));
+    console.log('TG Bot is up and running');
+    
+    vkBot.updates.start().catch(err => console.error('VK Bot launch error:', err));
+    console.log('VK Bot is up and running');
+
+    // Enable graceful stop
+    process.once('SIGINT', () => tgBot.stop('SIGINT'))
+    process.once('SIGTERM', () => tgBot.stop('SIGTERM'))
 }
 
 main()
