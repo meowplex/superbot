d5ee1e76fbb98267cb81425f707ae8a474a75390
kython-c <77231491+kython-c@users.noreply.github.com>
2021-10-18 21:18:35 +0300

    refactor: :zap: update weather command

--- src/superbot/actions/commands/weather.js
+++ src/superbot/actions/commands/weather.js
@@ -1,7 +1,15 @@
 import { Resp } from "../../../../lib/superbot/index.js";
-//import fetch from "node-fetch";
+import fetch from "node-fetch";
 
-export default function weather_command(context, cityName) {
-    let res = new Resp(`Hello, ${cityName}`)
+export default async function weather_command(context, cityName) {
+    let weatherData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHER_API_KEY}&lang=ru&units=metric`)
+    let weatherJson = await weatherData.json();
+    if (weatherJson.cod == "404"){
+        let res = new Resp(`Город не найден`)
+        context.reply(res)
+    }
+    let res = new Resp(`Погода в ${weatherJson.name}\n\tТемпература: ${weatherJson.main.temp} ℃\n\t` + 
+    `Ощущается как: ${weatherJson.main.feels_like} ℃\n\tСкорость ветра: ${weatherJson.wind.speed} м/с`
+    )
     context.reply(res)
 }
