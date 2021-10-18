f7306369d260c1073bf492ccb6ee4403c8f32e61
kython-c <77231491+kython-c@users.noreply.github.com>
2021-10-18 20:53:54 +0300

    refactor: :fire: remove model & services

--- tests/model/user.test.js
+++
@@ -1,9 +0,0 @@
-import { User } from "../../src/model/user.js";
-
-let user = new User(1, 'Nick');
-let json = user.toJSON();
-let user1 = User.fromJSON(json);
-
-test('user == user1', () => {
-    expect(user.toString()).toEqual(user1.toString());
-})
